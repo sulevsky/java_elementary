a161f133a121ce4b62be69e36119338d2109a35e
vladimir.sulevskiy <vsu@conciliolabs.com>
2017-12-04 18:41:58 +0200

    Lesson 4 Methods

---
+++ src/main/java/l4_functions/TestArrayChange.java
@@ -0,0 +1,24 @@
+package l4_functions;
+
+import java.util.Arrays;
+
+public class TestArrayChange {
+    public static void main(String[] args) {
+        int v = 10;
+        int[] a = {1,2,3};
+        changeData(v,a);
+        System.out.println(v);
+        System.out.println(Arrays.toString(a));
+        System.out.println();
+    }
+    public static void changeData(int var, int[] arr){
+        System.out.println(var);
+        System.out.println(Arrays.toString(arr));
+        System.out.println();
+        var = 0;
+        arr[1] =  100;
+        System.out.println(var);
+        System.out.println(Arrays.toString(arr));
+        System.out.println();
+    }
+}
---
+++ src/main/java/l4_functions/dynamicprogramming/SimpleFib.java
@@ -0,0 +1,67 @@
+package l4_functions.dynamicprogramming;
+
+/**
+ * Created by VSulevskiy on 28.06.2015.
+ */
+public class SimpleFib {
+    public static boolean f() {
+        System.out.println("In f");
+        return false;
+    }
+
+    public static boolean s() {
+        System.out.println("In s");
+        return true;
+    }
+
+    public static void main(String[] args) {
+        if ( s() | f()) {
+            System.out.println("In clause");
+        }
+//        for (int i = 1; i <= 10; i++) {
+//            System.out.println(i + " :\t " + getFibNumber(i));
+//        }
+//
+//        int loadTestNumber = 46; //45 - 10 sec, 46 - 15 sec
+//
+//        //first test
+//        Instant start1 = Instant.now();
+//        System.out.println(loadTestNumber + " :\t " + getFibNumber(loadTestNumber));
+//        Instant end1 = Instant.now();
+//        Duration duration1 = Duration.between(start1,end1);
+//        System.out.println("Calculation of " + loadTestNumber + " took " + duration1.toString());
+//
+//        //second test
+//        long[] cache = new long[loadTestNumber];
+//        long start = System.currentTimeMillis();
+//        System.out.println(loadTestNumber + " :\t " + getFibNumberDynamic(loadTestNumber, cache));
+//        long duration = System.currentTimeMillis() - start;
+//        System.out.println("Calculation of " + loadTestNumber + " took " + (double) duration / 1000 + " seconds");
+//
+
+    }
+
+    public static long getFibNumber(int index) {
+        if (index == 1 || index == 2) {
+            return 1;
+        } else {
+            return getFibNumber(index - 1) + getFibNumber(index - 2);
+        }
+    }
+
+    public static long getFibNumberDynamic(int index, long[] cache) {
+        long result;
+        if (cache[index - 1] != 0) {
+            result = cache[index - 1];
+        } else if (index == 1 || index == 2) {
+            result = 1;
+            cache[index - 1] = result;
+        } else {
+            result = getFibNumberDynamic(index - 1, cache) + getFibNumberDynamic(index - 2, cache);
+            cache[index - 1] = result;
+        }
+        return result;
+    }
+}
+
+
